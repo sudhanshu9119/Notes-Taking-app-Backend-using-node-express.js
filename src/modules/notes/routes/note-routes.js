9bfe552825f03fd0e9bfdcbce21d753a8c224d73
Sudhanshu Tiwari <126154604+sudhanshu9119@users.noreply.github.com>
2023-07-28 18:07:53 +0530

    Add files via upload

---
+++ src/modules/notes/routes/note-routes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import {NOTE_ROUTES} from "../../../shared/utils/app-constants.js";
+import noteController from '../controllers/note-controller.js';
+const noteRoutes = express.Router();
+
+noteRoutes.post(NOTE_ROUTES.ADD,noteController.addNote);
+noteRoutes.get(NOTE_ROUTES.GET_ALL_NOTES,noteController.getAllNotes );
+export default noteRoutes;
